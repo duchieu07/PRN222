e2aab59bdfaf2001bac67146c00d2d5e41aa8d22
duchieu07 <duchieu072003@gmail.com>
2025-01-15 09:33:00 +0700

    lab2

---
+++ Lab2_HttpClient/Lab2_HttpClient/MainWindow.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Net.Http;
+
+namespace Lab2_HttpClient
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        readonly HttpClient client = new HttpClient();
+        private void btnClse_Click(object sender, RoutedEventArgs e) => Close();
+        private void btnClear_Click(object sender, RoutedEventArgs e)
+        {
+            lbContent.Text = string.Empty;
+        }
+        private async void btnViewHTML_Click(object sender, RoutedEventArgs e)
+        {
+            string uri = lblURL.Text;
+            try
+            {
+                string responseBody = await client.GetStringAsync(uri);
+                lbContent.Text = responseBody.Trim();
+            }
+            catch (HttpRequestException ex) 
+            {
+                MessageBox.Show($"Message : {ex.Message}");
+            }
+        }
+    }
+}
